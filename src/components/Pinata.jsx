263970cdf4cee1bdf4e8b35a5c5d81cbcfcbc9c0
chupacambra <jovankoul@hotmail.com>
2023-07-19 21:47:45 +0300

    css added

--- src/components/Pinata.jsx
+++ src/components/Pinata.jsx
@@ -2,9 +2,24 @@ import "./pinata.css"
 
 const Pinata = () => {
   return (
-    <div className="gallow">
-
+    <div className="container">
+      <div className="hangman-container">
+        <div className="gallow"><div className="plank"></div></div>
+        <div className="head-container">
+          <div className="head"></div>
+        </div>
+        <div className="body">
+          <div className="handL"></div>
+          <div className="torso"></div>
+          <div className="handR"></div>
+        </div>
+        <div className="legs">
+          <div className="legL"></div>
+          <div className="legR"></div>
+        </div>
+      </div>
     </div>
+
   )
 }
 
